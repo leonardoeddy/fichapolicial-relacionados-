649b0ee8716ef180767f33f5e7efdb82ba4ebee2
Eddy Leonardo <106611396+leonardoeddy@users.noreply.github.com>
2023-03-24 20:04:29 -0400

    Add files via upload

---
+++ dbo.Relacionados.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Relacionados] (
+    [IdRelacionados] INT         IDENTITY (1, 1) NOT NULL,
+    [IdCiudadano]    INT         NOT NULL,
+    [Nombre ]        NCHAR (30)  NOT NULL,
+    [Apellidos]      NCHAR (30)  NULL,
+    [Parentesco]     NCHAR (15)  NULL,
+    [Dirección]      NCHAR (100) NULL,
+    [Estado]         NCHAR (10)  DEFAULT ('Activo') NOT NULL,
+    PRIMARY KEY CLUSTERED ([IdRelacionados] ASC),
+    CONSTRAINT [FK_Relacionados_Ciudadano] FOREIGN KEY ([IdCiudadano]) REFERENCES [dbo].[Relacionados] ([IdRelacionados])
+);
+
